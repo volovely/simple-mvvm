7618e38996348253c3bbc7205ecd828dfa4bce47
Roman Volovelskiy <romanv@y-tree.com>
2022-02-02 18:21:14 +0200

    Added pods

--- simple-mvvm/ViewController.swift
+++ simple-mvvm/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  simple-mvvm
-//
-//  Created by roman on 02.02.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
 
