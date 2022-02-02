9f27ce524cc6e13d7bb587b3094b9ea4f7ba4795
Roman Volovelskiy <romanv@y-tree.com>
2022-02-02 18:17:35 +0200

    Initial Commit

---
+++ simple-mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple-mvvm
+//
+//  Created by roman on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
